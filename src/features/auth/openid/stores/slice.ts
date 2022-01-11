3475e0f8763439261177a2ea6f0c17ddd9d93ae3
Johndoe <56298669+togane@users.noreply.github.com>
2022-01-12 06:23:49 +0900

    fix auth slice

--- src/features/auth/openid/stores/slice.ts
+++ src/features/auth/openid/stores/slice.ts
@@ -1,20 +1,34 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import isEmpty from 'lodash/isEmpty';
+
+import { AuthToken } from '../';
 
 export type State = {
   isAuthenticated: boolean;
+  accessToken: string;
+  refreshToken: string;
+  accessTokenExpiresAt: Date | undefined;
+  refreshTokenExpiresAt: Date | undefined;
 };
-
 export const initialState: State = {
   isAuthenticated: false,
+  accessToken: '',
+  refreshToken: '',
+  accessTokenExpiresAt: undefined,
+  refreshTokenExpiresAt: undefined,
 };
 
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    createSession(state) {
-      state.isAuthenticated = true;
-      return state;
+    updateTokens: (state, action: PayloadAction<{ authToken: AuthToken }>) => {
+      const { authToken } = action.payload;
+
+      return Object.assign(state, {
+        ...authToken,
+        isAuthenticated: !isEmpty(authToken.accessToken),
+      });
     },
   },
 });
