9be43d608280c2c35cecb8baf178924ae26280ae
Johndoe <56298669+togane@users.noreply.github.com>
2022-01-12 20:14:25 +0900

    implement callback for oauth2.0

--- src/features/auth/openid/components/Callback/Container/index.tsx
+++ src/features/auth/openid/components/Callback/Container/index.tsx
@@ -1,14 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router';
+
+import {
+  authStore,
+  CLIENT_ID,
+  createAuthTokenFromTokenResponse,
+  createToken,
+  TokenRequest,
+  Url,
+} from '@/features/auth/openid';
+import { useAppDispatch } from '@/stores/store';
+import storage from '@/utils/storage';
 
 export const Container: React.VFC = () => {
+  const dispatch = useAppDispatch();
+  const location = useLocation();
   const navigate = useNavigate();
-  const _query = new URLSearchParams(location.search);
-  const state = _query.get('state') as string;
+
   const [loading, setLoading] = useState(true);
 
+  const _query = new URLSearchParams(location.search);
+  const state = _query.get('state') as string;
+  const persistedState = storage.getState();
+
+  const params: TokenRequest = {
+    grant_type: 'authorization_code',
+    scope: 'openid',
+    code: _query.get('code') as string,
+    client_id: CLIENT_ID,
+    code_verifier: storage.getCodeVerifier(),
+    redirect_uri: Url.redirectUri,
+  };
+
   useEffect(() => {
-    alert(state);
+    if (state === persistedState) {
+      storage.clearState();
+    } else {
+      // TODO: UI/UX設計の上実装
+      alert('CSRF detected.');
+      return;
+    }
+
+    (async () => {
+      // TODO: IDトークンの検証(nonceの確認もここ)
+      const [tokenResponse] = await createToken(params);
+      const authToken = createAuthTokenFromTokenResponse(tokenResponse);
+      dispatch(authStore.actions.updateTokens({ authToken: authToken }));
+    })();
+
+    setLoading(false);
     return;
   }, []);
 
@@ -16,7 +56,7 @@ export const Container: React.VFC = () => {
   if (loading) {
     return <React.Fragment />;
   } else {
-    navigate('/');
+    navigate('/dashboard');
     return <React.Fragment />;
   }
 };
