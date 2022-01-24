04aeb3483517859ffb8f7816270ea2c904ac0a25
Johndoe <56298669+togane@users.noreply.github.com>
2022-01-25 06:07:25 +0900

    add a constant to config module

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,3 +5,6 @@ export const OAUTH_REDIRECT_URI = process.env
   .REACT_APP_OAUTH_REDIRECT_URI as string;
 export const OPENID_PROVIDER_BASE_URL = process.env
   .REACT_APP_OPENID_PROVIDER_BASE_URL as string;
+
+export const GRAPHQL_SERVER_BASE_URL = process.env
+  .REACT_APP_GRAPHQL_SERVER_BASE_URL as string;
